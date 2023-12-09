6b0bb16771de1d47bf26e77a79ebb2719bd2d52c
Fabian <50114802+fabalexsie@users.noreply.github.com>
2023-12-09 23:52:25 +0100

    Layout for buttons in BottomSheet

--- app/src/main/java/de/siebes/fabian/virtucard/MainActivity.kt
+++ app/src/main/java/de/siebes/fabian/virtucard/MainActivity.kt
@@ -7,30 +7,47 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberBottomSheetScaffoldState
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import de.siebes.fabian.virtucard.ui.theme.VirtuCardTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,7 +65,7 @@ class MainActivity : ComponentActivity() {
 fun MainContent() {
     val scaffoldState = rememberBottomSheetScaffoldState(
         bottomSheetState = SheetState(
-            initialValue = SheetValue.PartiallyExpanded,
+            initialValue = SheetValue.Expanded,
             skipPartiallyExpanded = false,
             skipHiddenState = true,
         )
@@ -94,27 +111,71 @@ fun MyWebView() {
 @Composable
 fun MyBottomSheet(
 ) {
+    var shareUrl by remember { mutableStateOf("") }
+
+    val vSpaceDp = 15.dp
+
     Column(
         Modifier
             .fillMaxWidth()
-            .padding(15.dp, bottom = 50.dp)
+            .padding(horizontal = 15.dp)
     ) {
-        // Sheet content
-        // QR-Code, Nearby, ...
-        Button(onClick = { }) {
-            Text(text = "Hello World!")
+        // Nearby, ...
+        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            OutlinedButton(onClick = { }, modifier = Modifier.padding(horizontal = 5.dp)) {
+                Icon(
+                    painter = painterResource(id = R.drawable.nearby_share),
+                    contentDescription = "Nearby Icon",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(modifier = Modifier.width(ButtonDefaults.IconSpacing))
+                Text(text = "Nearby")
+            }
+            OutlinedButton(onClick = { }, modifier = Modifier.padding(horizontal = 5.dp)) {
+                // use drawable inside of icon
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_content_copy_24),
+                    contentDescription = "Copy Icon",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(modifier = Modifier.width(ButtonDefaults.IconSpacing))
+                Text(text = "Copy")
+            }
+            OutlinedButton(onClick = { }, modifier = Modifier.padding(horizontal = 5.dp)) {
+                Icon(
+                    Icons.Filled.Share,
+                    contentDescription = "Share Icon",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(modifier = Modifier.width(ButtonDefaults.IconSpacing))
+                Text(text = "Share")
+            }
         }
-        Divider(Modifier.padding(vertical = 15.dp), thickness = 1.dp)
+
+        Divider(Modifier.padding(vertical = vSpaceDp), thickness = 1.dp)
+
+        // QR-Code, ...
         Button(onClick = {}) {
             Text(text = "Hello World!")
         }
+
+        Spacer(modifier = Modifier.height(vSpaceDp))
+        TextField(
+            value = shareUrl,
+            onValueChange = { shareUrl = it },
+            label = { Text("Share URL") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(vSpaceDp))
         Text(
             text = "Created by Fabian Siebes",
             Modifier
-                .padding(25.dp)
                 .fillMaxWidth(),
             textAlign = TextAlign.Center,
         )
+
+        Spacer(modifier = Modifier.height(vSpaceDp))
     }
 }
 
@@ -122,8 +183,6 @@ fun MyBottomSheet(
 @Preview(showBackground = true)
 @Composable
 fun BottomSheetPreview() {
-    val sheetState = rememberModalBottomSheetState()
-
     VirtuCardTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
